8b87e4181306ee7944720118984759dd5635987f
tony3sord <tressordtony@gmail.com>
2025-04-24 15:07:02 -0400

    add: Integrar ParkingRepository y configurar TypeOrmModule en ParkingModule

--- src/modules/parking/parking.module.ts
+++ src/modules/parking/parking.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { ParkingService } from './service/parking.service';
 import { ParkingController } from './controller/parking.controller';
+import { ParkingRepository } from './repository/parking.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Parking } from './entities/parking.entity';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature(
+      [Parking],
+      process.env.POSTGRES_DB_CONNECTION_NAME,
+    ),
+  ],
   controllers: [ParkingController],
-  providers: [ParkingService],
+  providers: [ParkingService, ParkingRepository],
 })
 export class ParkingModule {}
