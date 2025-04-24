7fb6da5200b3c16234fba92f7be396db890c2d78
tony3sord <tressordtony@gmail.com>
2025-04-23 21:35:50 -0400

    refactor: simplify database configuration for Postgres and MongoDB

--- src/common/database/typeorm.config.ts
+++ src/common/database/typeorm.config.ts
@@ -1,41 +1,32 @@
-import { DataSource } from 'typeorm';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
-export const databaseProviders = [
-  // Configuration for Postgres
-  {
-    provide: 'POSTGRES_DATA_SOURCE',
-    useFactory: async () => {
-      const dataSourcePostgres = new DataSource({
-        type: 'postgres',
-        host: process.env.POSTGRES_DB_HOST,
-        port: parseInt(process.env.POSTGRES_DB_PORT as string) || 5432,
-        username: process.env.POSTGRES_DB_USER,
-        password: process.env.POSTGRES_DB_PASSWORD,
-        database: process.env.POSTGRES_DB_NAME,
-        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-        synchronize: process.env.NODE_ENV === 'production' ? false : true,
-      });
-
-      return dataSourcePostgres.initialize();
-    },
+export const databaseProviders: {
+  postgres: TypeOrmModuleOptions;
+  mongodb: TypeOrmModuleOptions;
+} = {
+  // Configuración para Postgres
+  postgres: {
+    type: 'postgres',
+    host: process.env.POSTGRES_DB_HOST,
+    port: parseInt(process.env.POSTGRES_DB_PORT as string) || 5432,
+    username: process.env.POSTGRES_DB_USER,
+    password: process.env.POSTGRES_DB_PASSWORD,
+    database: process.env.POSTGRES_DB_NAME,
+    entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+    synchronize: process.env.NODE_ENV === 'production' ? false : true,
   },
 
-  // Configuration for MongoDB
-  {
-    provide: 'MONGODB_DATA_SOURCE',
-    useFactory: async () => {
-      const dataSourceMongo = new DataSource({
-        type: 'mongodb',
-        host: process.env.MONGO_DB_HOST,
-        port: parseInt(process.env.MONGO_DB_PORT as string) || 27017,
-        username: process.env.MONGO_DB_USER,
-        password: process.env.MONGO_DB_PASSWORD,
-        database: process.env.MONGO_DB_NAME,
-        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-        synchronize: process.env.NODE_ENV === 'production' ? false : true,
-      });
-
-      return dataSourceMongo.initialize();
-    },
+  // Configuración para MongoDB
+  mongodb: {
+    type: 'mongodb',
+    host: process.env.MONGO_DB_HOST,
+    port: parseInt(process.env.MONGO_DB_PORT as string) || 27017,
+    // username: process.env.MONGO_DB_USER,
+    // password: process.env.MONGO_DB_PASSWORD,
+    database: process.env.MONGO_DB_NAME,
+    entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+    synchronize: process.env.NODE_ENV === 'production' ? false : true,
   },
-];
+};
