5947a8ef3fb2680f8a7264f837cbdf2efadf9eb3
tony3sord <tressordtony@gmail.com>
2025-04-24 14:35:49 -0400

    add: Update UpdateUserDto to make fields optional and field phone number

--- src/modules/user/dto/update-user.dto.ts
+++ src/modules/user/dto/update-user.dto.ts
@@ -1,28 +1,34 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsIn, IsString } from 'class-validator';
+import { IsEmail, IsIn, IsOptional, IsString } from 'class-validator';
 
 export class UpdateUserDto {
   @ApiProperty({ description: 'The name of the user.' })
   @IsString()
-  readonly name: string;
+  @IsOptional()
+  readonly name?: string;
 
   @ApiProperty({ description: 'The lastname of the user.' })
   @IsString()
-  readonly lastname: string;
+  @IsOptional()
+  readonly lastname?: string;
 
   @ApiProperty({ description: 'The username of the user.' })
-  @IsString({ message: 'Hola' })
-  readonly username: string;
+  @IsString()
+  @IsOptional()
+  readonly username?: string;
 
   @ApiProperty({ description: 'The email of the user.' })
   @IsEmail()
-  readonly email: string;
+  @IsOptional()
+  readonly email?: string;
 
-  @ApiProperty({ description: 'The password of the user.' })
+  @ApiProperty({ description: 'The phone number of the user.' })
   @IsString()
-  readonly password: string;
+  @IsOptional()
+  readonly phone?: string;
 
   @ApiProperty({ description: 'The role of the user.' })
   @IsIn(['Admin', 'Client', 'Worker'])
-  readonly role: 'Admin' | 'Client' | 'Worker';
+  @IsOptional()
+  readonly role?: 'Admin' | 'Client' | 'Worker';
 }
