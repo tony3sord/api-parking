ede2afee023cc4641d8652951398b5fc0d1de165
tony3sord <tressordtony@gmail.com>
2025-04-24 09:15:57 -0400

    add: Implement DTO and user entity with validations and properties

--- src/modules/user/dto/create-user.dto.ts
+++ src/modules/user/dto/create-user.dto.ts
@@ -1,1 +1,28 @@
-export class CreateUserDto {}
+import { IsEmail, IsString, IsIn } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateUserDto {
+  @ApiProperty({ description: 'El nombre del usuario.' })
+  @IsString()
+  readonly name: string;
+
+  @ApiProperty({ description: 'El apellido del usuario.' })
+  @IsString()
+  readonly lastname: string;
+
+  @ApiProperty({ description: 'El nombre de usuario del usuario.' })
+  @IsString()
+  readonly user: string;
+
+  @ApiProperty({ description: 'El correo del usuario.' })
+  @IsEmail()
+  readonly email: string;
+
+  @ApiProperty({ description: 'La contraseña del usuario.' })
+  @IsString()
+  readonly password: string;
+
+  @ApiProperty({ description: 'El rol del usuario.' })
+  @IsIn(['Admin', 'Client', 'SuperAdmin'])
+  readonly role: 'Admin' | 'Client' | 'SuperAdmin';
+}
--- src/modules/user/entities/user.entity.ts
+++ src/modules/user/entities/user.entity.ts
@@ -1,1 +1,25 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Usuario {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  lastname: string;
+
+  @Column()
+  username: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  role: string;
+}
