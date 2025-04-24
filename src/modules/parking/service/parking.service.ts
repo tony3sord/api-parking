31e414b0cb1f5a8b8a1cc5c6c1ec5d5fb0db8941
tony3sord <tressordtony@gmail.com>
2025-04-24 15:07:07 -0400

    add: Integrar ParkingRepository en ParkingService para manejar solicitudes de estacionamiento

--- src/modules/parking/service/parking.service.ts
+++ src/modules/parking/service/parking.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { CreateParkingDto } from '../dto/create-parking.dto';
 import { UpdateParkingDto } from '../dto/update-parking.dto';
+import { ParkingRepository } from '../repository/parking.repository';
 
 @Injectable()
 export class ParkingService {
-  create(createParkingDto: CreateParkingDto) {
-    return 'This action adds a new parking';
+  constructor(private readonly parkingRepository: ParkingRepository) {}
+
+  async create(createParkingDto: CreateParkingDto) {
+    return await this.parkingRepository.reqParkCar(createParkingDto);
   }
 
   findAll() {
