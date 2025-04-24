6b1fc938c92a8af3d8f9f2a003e06e7b850ddca0
tony3sord <tressordtony@gmail.com>
2025-04-24 13:02:21 -0400

    add: Enable CORS and set up Swagger documentation in main application file

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule } from '@nestjs/swagger';
+import { configSwagger } from './common/config/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: process.env.CORS_ORIGIN,
+    methods: 'GET,PATCH,POST,DELETE',
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
     }),
   );
+  const document = SwaggerModule.createDocument(app, configSwagger);
+  SwaggerModule.setup('/', app, document);
 
   const port = process.env.PORT ?? 3000;
   const server = await app.listen(port);
