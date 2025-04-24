482d4dee9410a5107550b6f0a99afbb4cda4fafa
tony3sord <tressordtony@gmail.com>
2025-04-23 21:36:35 -0400

    add: Configure TypeORM for Postgres and MongoDB in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ParkingModule } from './modules/parking/parking.module';
+import { ConfigModule } from '@nestjs/config';
+import { databaseProviders } from './common/database/typeorm.config';
 
 @Module({
-  imports: [ParkingModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    // Configuration for Postgres
+    TypeOrmModule.forRootAsync({
+      name: 'postgresConnection',
+      useFactory: () => databaseProviders.postgres,
+    }),
+    // Configuration for MongoDB
+    TypeOrmModule.forRootAsync({
+      name: 'mongoConnection',
+      useFactory: () => databaseProviders.mongodb,
+    }),
+    ParkingModule,
+  ],
 })
 export class AppModule {}
