8bcbec9c6392933cf4feba8f1372a54bc867d20b
tony3sord <tressordtony@gmail.com>
2025-04-24 10:24:58 -0400

    add: Implement UserRepository with createUser method and password hashing

---
+++ src/modules/user/repository/user.repository.ts
@@ -0,0 +1,21 @@
+import { EntityManager } from 'typeorm';
+import { User } from '../entities/user.entity';
+import { CreateUserDto, UpdateUserDto } from '../dto';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class UserRepository {
+  constructor(private dataSource: EntityManager) {}
+
+  async createUser(usuario: CreateUserDto): Promise<User> {
+    const userRepository = this.dataSource.getRepository(User);
+
+    const usuarioNew = userRepository.create({
+      ...usuario,
+      password: await bcrypt.hash(usuario.password, 10),
+    });
+
+    return userRepository.save(usuarioNew);
+  }
+}
