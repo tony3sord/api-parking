f973566f338547a3dd8280588e98013f49704cb2
tony3sord <tressordtony@gmail.com>
2025-04-26 01:35:22 -0400

    add: Integrate JwtModule into AuthModule for JWT-based authentication

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './service/auth.service';
 import { AuthController } from './controller/auth.controller';
 import { UserModule } from '../user/user.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: process.env.JWT_EXPIRATION },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
