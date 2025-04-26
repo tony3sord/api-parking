10296d6613a401b52857a8ba6247e7aeb5dd541e
tony3sord <tressordtony@gmail.com>
2025-04-26 03:28:14 -0400

    add: Add examples to the API documentation for creating and updating users in CreateUserDto and UpdateUserDto

--- src/modules/user/dto/update-user.dto.ts
+++ src/modules/user/dto/update-user.dto.ts
@@ -2,32 +2,44 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsIn, IsOptional, IsString } from 'class-validator';
 
 export class UpdateUserDto {
-  @ApiProperty({ description: 'The name of the user.' })
+  @ApiProperty({ description: 'The name of the user.', example: 'Tony' })
   @IsString()
   @IsOptional()
   readonly name?: string;
 
-  @ApiProperty({ description: 'The lastname of the user.' })
+  @ApiProperty({ description: 'The lastname of the user.', example: 'Aliaga' })
   @IsString()
   @IsOptional()
   readonly lastname?: string;
 
-  @ApiProperty({ description: 'The username of the user.' })
+  @ApiProperty({
+    description: 'The username of the user.',
+    example: 'tony3sord',
+  })
   @IsString()
   @IsOptional()
   readonly username?: string;
 
-  @ApiProperty({ description: 'The email of the user.' })
+  @ApiProperty({
+    description: 'The email of the user.',
+    example: 'tony@gmail.com',
+  })
   @IsEmail()
   @IsOptional()
   readonly email?: string;
 
-  @ApiProperty({ description: 'The phone number of the user.' })
+  @ApiProperty({
+    description: 'The phone number of the user.',
+    example: '+53 55964629',
+  })
   @IsString()
   @IsOptional()
   readonly phone?: string;
 
-  @ApiProperty({ description: 'The role of the user.' })
+  @ApiProperty({
+    description: 'The role of the user.',
+    example: 'Worker',
+  })
   @IsIn(['Admin', 'Client', 'Worker'])
   @IsOptional()
   readonly role?: 'Admin' | 'Client' | 'Worker';
