2729915f964a55d1838e88e71889875249423bc5
tony3sord <tressordtony@gmail.com>
2025-04-26 03:08:27 -0400

    add: Implement conflict handling in create method to prevent overlapping parking reservations

--- src/modules/parking/service/parking.service.ts
+++ src/modules/parking/service/parking.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { CreateParkingDto } from '../dto/create-parking.dto';
 import { UpdateParkingDto } from '../dto/update-parking.dto';
 import { ParkingRepository } from '../repository/parking.repository';
@@ -8,6 +8,16 @@ export class ParkingService {
   constructor(private readonly parkingRepository: ParkingRepository) {}
 
   async create(createParkingDto: CreateParkingDto) {
+    if (
+      await this.parkingRepository.getStateParkingForReservationDate(
+        createParkingDto,
+      )
+    ) {
+      throw new ConflictException(
+        'Parking already exists for the given date and time',
+      );
+    }
+
     return await this.parkingRepository.reqParkCar(createParkingDto);
   }
 
