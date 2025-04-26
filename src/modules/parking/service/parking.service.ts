de9a4907c2190fd15d5da0b7f8e2f2df5b3cfa94
tony3sord <tressordtony@gmail.com>
2025-04-26 03:59:50 -0400

    add: Modify create method in ParkingService to include user information from JWT token

--- src/modules/parking/service/parking.service.ts
+++ src/modules/parking/service/parking.service.ts
@@ -2,12 +2,22 @@ import { ConflictException, Injectable } from '@nestjs/common';
 import { CreateParkingDto } from '../dto/create-parking.dto';
 import { UpdateParkingDto } from '../dto/update-parking.dto';
 import { ParkingRepository } from '../repository/parking.repository';
+import { Parking } from '../entities/parking.entity';
+import { JwtService } from '@nestjs/jwt';
+import { UserRepository } from 'src/modules/user/repository/user.repository';
 
 @Injectable()
 export class ParkingService {
-  constructor(private readonly parkingRepository: ParkingRepository) {}
+  constructor(
+    private readonly parkingRepository: ParkingRepository,
+    private readonly jwtService: JwtService,
+    private readonly userRepository: UserRepository,
+  ) {}
 
-  async create(createParkingDto: CreateParkingDto) {
+  async create(
+    createParkingDto: CreateParkingDto,
+    token: string,
+  ): Promise<Parking> {
     if (
       await this.parkingRepository.getStateParkingForReservationDate(
         createParkingDto,
@@ -17,8 +27,10 @@ export class ParkingService {
         'Parking already exists for the given date and time',
       );
     }
+    const decodedToken = this.jwtService.decode(token);
+    const user = await this.userRepository.getForId(decodedToken.idUser);
 
-    return await this.parkingRepository.reqParkCar(createParkingDto);
+    return await this.parkingRepository.reqParkCar(createParkingDto, user);
   }
 
   async getState(): Promise<boolean> {
