9e1061cfb3b25287ec7a7a1e91419e9eaaafeb57
tony3sord <tressordtony@gmail.com>
2025-04-28 02:57:17 -0400

    add: Include user deletion end-to-end test in custom test sequencer

--- test/test.sequencer.ts
+++ test/test.sequencer.ts
@@ -8,6 +8,7 @@ class CustomSequencer extends Sequencer {
       'auth.controller.e2e-spec.ts',
       'user.update.e2e-spec.ts',
       'parking.controller.e2e-spec.ts',
+      'user.delete.e2e-spec.ts',
     ];
 
     console.log(
