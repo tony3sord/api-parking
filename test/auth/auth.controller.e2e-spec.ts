12d7e9b368fe34919fd8f60c0a8dd40d04fd3448
tony3sord <tressordtony@gmail.com>
2025-04-28 02:48:23 -0400

    rename: Move AuthController end-to-end test to a new directory and update imports

--- test/auth/auth.controller.e2e-spec.ts
+++ test/auth/auth.controller.e2e-spec.ts
@@ -1,10 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from '../../../app.module';
-import { loginDtoTest } from '../../../../test/auth/login.objects';
+import { AppModule } from '../../src/app.module';
+import { loginDtoTest } from './login.objects';
 
 export const tokensByRole: Record<string, string> = {};
+
+const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 describe('AuthController (e2e)', () => {
   let app: INestApplication;
@@ -31,12 +33,13 @@ describe('AuthController (e2e)', () => {
     await app.close();
   });
 
-  it('/api/auth/login (POST)', async () => {
+  it('/api/auth/login (POST) - should log in and obtain token', async () => {
+    await wait(3000);
     for (const user of loginDtoTest) {
       const response = await request(app.getHttpServer())
         .post('/api/auth/login')
         .send({ identifier: user.identifier, password: user.password })
-        .expect(201); //201 login
+        .expect(201); // 201 login
 
       if (response.status === 201) {
         expect(response.body).toEqual(
