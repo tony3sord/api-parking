ea8400e555e1c705c05496b477afd7006e85e98b
tony3sord <tressordtony@gmail.com>
2025-04-29 16:34:56 -0400

    feat: Add parking parameter to reqParkCar method and handle parking not found in ParkingSpotService

--- src/modules/parkingSpot/service/parkingSpot.service.ts
+++ src/modules/parkingSpot/service/parkingSpot.service.ts
@@ -1,16 +1,22 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateParkingSpotDto, UpdateParkingSpotDto } from '../dto/index';
 import { ParkingSpotRepository } from '../repository/parkingSpot.repository';
 import { ParkingSpot } from '../entities/parkingSpot.entity';
 import { JwtService } from '@nestjs/jwt';
 import { UserRepository } from '../../user/repository/user.repository';
+import { ParkingService } from 'src/modules/parking/service/parking.service';
 
 @Injectable()
 export class ParkingSpotService {
   constructor(
-    private readonly parkingRepository: ParkingSpotRepository,
+    private readonly parkingSpotRepository: ParkingSpotRepository,
     private readonly jwtService: JwtService,
     private readonly userRepository: UserRepository,
+    private readonly parkingService: ParkingService,
   ) {}
 
   async create(
@@ -18,7 +24,7 @@ export class ParkingSpotService {
     token: string,
   ): Promise<ParkingSpot> {
     if (
-      await this.parkingRepository.getStateParkingForReservationDate(
+      await this.parkingSpotRepository.getStateParkingForReservationDate(
         createParkingSpotDto,
       )
     ) {
@@ -26,22 +32,32 @@ export class ParkingSpotService {
         'Parking already exists for the given date and time',
       );
     }
+    const parking = await this.parkingService.findOne(
+      createParkingSpotDto.parking,
+    );
+    if (!parking) {
+      throw new NotFoundException('This parking not found');
+    }
     const decodedToken = this.jwtService.decode(token);
     const user = await this.userRepository.getForId(decodedToken.idUser);
 
-    return await this.parkingRepository.reqParkCar(createParkingSpotDto, user);
+    return await this.parkingSpotRepository.reqParkCar(
+      createParkingSpotDto,
+      user,
+      parking,
+    );
   }
 
   async getState(): Promise<boolean> {
-    return await this.parkingRepository.getStateNow();
+    return await this.parkingSpotRepository.getStateNow();
   }
 
   async createParkingSpace(): Promise<boolean> {
-    return await this.parkingRepository.getStateNow();
+    return await this.parkingSpotRepository.getStateNow();
   }
 
   async getLogs(): Promise<ParkingSpot[]> {
-    return await this.parkingRepository.getLogsForAdmin();
+    return await this.parkingSpotRepository.getLogsForAdmin();
   }
 
   findOne(id: number) {
