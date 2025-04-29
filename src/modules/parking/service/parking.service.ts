dc3f815f9c8841ead4f3d23be501bfdf995a5136
tony3sord <tressordtony@gmail.com>
2025-04-29 00:48:20 -0400

    feat: Add error handling to create, findAll, and findOne methods in ParkingService

--- src/modules/parking/service/parking.service.ts
+++ src/modules/parking/service/parking.service.ts
@@ -6,21 +6,36 @@ import { ParkingRepository } from '../repository/parking.repository';
 export class ParkingService {
   constructor(private readonly parkingRepository: ParkingRepository) {}
   async create(createParkingDto: CreateParkingDto) {
-    const validation = await this.parkingRepository.getParkingForName(
-      createParkingDto.name,
-    );
-    if (validation) {
-      throw new ConflictException('Parking already exists');
+    try {
+      const validation = await this.parkingRepository.getParkingForName(
+        createParkingDto.name,
+      );
+      if (validation) {
+        throw new ConflictException('Parking already exists');
+      }
+      return await this.parkingRepository.createParking(createParkingDto);
+    } catch (error) {
+      console.log(error);
+      throw error;
     }
-    return await this.parkingRepository.createParking(createParkingDto);
   }
 
   async findAll() {
-    return await this.parkingRepository.getParkings();
+    try {
+      return await this.parkingRepository.getParkings();
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
   }
 
   async findOne(id: number) {
-    return `This action returns a #${id} parking`;
+    try {
+      return await this.parkingRepository.getParking(id);
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
   }
 
   async update(id: number, updateParkingDto: UpdateParkingDto) {
