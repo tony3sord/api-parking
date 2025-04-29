07bc16a51c2a83e91c56a3ad7efe47716e7966a0
tony3sord <tressordtony@gmail.com>
2025-04-29 00:16:24 -0400

    feat: Enhance ParkingController with Swagger documentation and role-based access for creating parking

--- src/modules/parking/controller/parking.controller.ts
+++ src/modules/parking/controller/parking.controller.ts
@@ -9,13 +9,39 @@ import {
 } from '@nestjs/common';
 import { ParkingService } from '../service/parking.service';
 import { CreateParkingDto, UpdateParkingDto } from '../dto/index';
+import { Roles } from 'src/common/decorators/roles.decorators';
+import { RolesEnum } from 'src/common/enums/roles.enum';
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Parking')
 @Controller('parking')
 export class ParkingController {
   constructor(private readonly parkingService: ParkingService) {}
 
   @Post()
-  create(@Body() createParkingDto: CreateParkingDto) {
+  @Roles(RolesEnum.Client)
+  @ApiOperation({
+    summary: 'Create a new parking',
+    description: 'Allows a client to create a new parking.',
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'The parking has been successfully created.',
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Unauthorized. Token is missing or invalid.',
+  })
+  @ApiResponse({
+    status: 409,
+    description:
+      'Conflict. A parking with the specified details already exists.',
+  })
+  @ApiBody({
+    description: 'The data required to create a parking',
+    type: CreateParkingDto,
+  })
+  async create(@Body() createParkingDto: CreateParkingDto) {
     return this.parkingService.create(createParkingDto);
   }
 
