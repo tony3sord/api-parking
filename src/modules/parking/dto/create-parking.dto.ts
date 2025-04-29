6a2c4b210f4e7236aa42953e70336f2e99901c97
tony3sord <tressordtony@gmail.com>
2025-04-29 00:29:53 -0400

    feat: Add validation and Swagger documentation to CreateParkingDto

--- src/modules/parking/dto/create-parking.dto.ts
+++ src/modules/parking/dto/create-parking.dto.ts
@@ -1,1 +1,20 @@
-export class CreateParkingDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsPositive, IsString } from 'class-validator';
+
+export class CreateParkingDto {
+  @ApiProperty({
+    description: 'The name of the parking',
+    example: 'Downtown Parking Lot',
+  })
+  @IsString()
+  name: string;
+
+  @ApiProperty({
+    description:
+      'The maximum capacity of the parking (number of vehicles it can hold)',
+    example: 50,
+  })
+  @IsNumber()
+  @IsPositive()
+  ability: number;
+}
