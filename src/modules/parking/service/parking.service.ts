696169118018ccee0d5399a1ee23abcff495d7cc
tony3sord <tressordtony@gmail.com>
2025-04-29 00:40:53 -0400

    feat: Add conflict handling to createParking method in ParkingService

--- src/modules/parking/service/parking.service.ts
+++ src/modules/parking/service/parking.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { CreateParkingDto, UpdateParkingDto } from '../dto/index';
 import { ParkingRepository } from '../repository/parking.repository';
 
@@ -6,7 +6,13 @@ import { ParkingRepository } from '../repository/parking.repository';
 export class ParkingService {
   constructor(private readonly parkingRepository: ParkingRepository) {}
   async create(createParkingDto: CreateParkingDto) {
-    return 'This action adds a new parking';
+    const validation = await this.parkingRepository.getParkingForName(
+      createParkingDto.name,
+    );
+    if (validation) {
+      throw new ConflictException('Parking already exists');
+    }
+    return await this.parkingRepository.createParking(createParkingDto);
   }
 
   async findAll() {
