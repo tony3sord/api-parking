0f5458b0da04916523dd516b6edb18df022bb980
tony3sord <tressordtony@gmail.com>
2025-04-29 00:03:56 -0400

    feat: Implement ParkingRepository for managing parking data

---
+++ src/modules/parking/repository/parkingSpot.repository.ts
@@ -0,0 +1,18 @@
+import { EntityManager } from 'typeorm';
+import { Parking } from '../entities/parking.entity';
+import { Injectable } from '@nestjs/common';
+import { InjectEntityManager } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+@Injectable()
+export class ParkingRepository {
+  constructor(
+    @InjectEntityManager(process.env.POSTGRES_DB_CONNECTION_NAME)
+    private readonly dataSource: EntityManager,
+  ) {}
+  async getParking(): Promise<Parking[]> {
+    const parkRepository = this.dataSource.getRepository(Parking);
+    return await parkRepository.find();
+  }
+}
