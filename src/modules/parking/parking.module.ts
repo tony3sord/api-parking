23af72ce7641729498428b6afc4c337818aeed06
tony3sord <tressordtony@gmail.com>
2025-04-30 02:42:36 -0400

    feat: Integrar Integrate JwtModule into ParkingModule for authentication

--- src/modules/parking/parking.module.ts
+++ src/modules/parking/parking.module.ts
@@ -2,10 +2,17 @@ import { Module } from '@nestjs/common';
 import { ParkingService } from './service/parking.service';
 import { ParkingController } from './controller/parking.controller';
 import { ParkingRepository } from './repository/parking.repository';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   controllers: [ParkingController],
   providers: [ParkingService, ParkingRepository],
   exports: [ParkingService],
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: process.env.JWT_EXPIRATION },
+    }),
+  ],
 })
 export class ParkingModule {}
