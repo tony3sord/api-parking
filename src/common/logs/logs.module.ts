035bf58b6e303479362fa7e6841f5ea79293449c
tony3sord <tressordtony@gmail.com>
2025-05-05 02:53:03 -0400

    feat: Import AuthModule in LogModule

--- src/common/logs/logs.module.ts
+++ src/common/logs/logs.module.ts
@@ -1,9 +1,11 @@
 // log.module.ts
 import { Module } from '@nestjs/common';
 import { LogService } from './logs.service';
+import { AuthModule } from '../../modules/auth/auth.module';
 
 @Module({
   providers: [LogService],
   exports: [LogService],
+  imports: [AuthModule],
 })
 export class LogModule {}
