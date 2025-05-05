acaab01b62dcc7aff485a804f8eb9b96056495ed
tony3sord <tressordtony@gmail.com>
2025-05-05 03:45:19 -0400

    fix: Remove obsolete parking controller tests and update test sequencer

--- test/parking/create.parking.objects.ts
+++
@@ -1,5 +0,0 @@
-export const createParkingDTOTest = {
-  vehicleDetails: 'Toyota Corolla 2022',
-  reservationDate: '2025-04-26T12:12:00Z',
-  reservationTime: 7200,
-};
--- test/test.sequencer.ts
+++ test/test.sequencer.ts
@@ -6,8 +6,9 @@ class CustomSequencer extends Sequencer {
       'user.controller.e2e-spec.ts',
       'app.e2e-spec.ts',
       'auth.controller.e2e-spec.ts',
-      'user.update.e2e-spec.ts',
       'parking.controller.e2e-spec.ts',
+      'user.update.e2e-spec.ts',
+      'parkingSpot.controller.e2e-spec.ts',
       'user.delete.e2e-spec.ts',
     ];
 
