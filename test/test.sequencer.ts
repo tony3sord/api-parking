202f1187cfdf4d9493c90b22400a8c93ea97a948
tony3sord <tressordtony@gmail.com>
2025-05-05 05:04:13 -0400

    test: Update test sequencer to reorder and include new test files

--- test/test.sequencer.ts
+++ test/test.sequencer.ts
@@ -3,20 +3,26 @@ const { default: Sequencer } = require('@jest/test-sequencer');
 class CustomSequencer extends Sequencer {
   sort(tests) {
     const order = [
-      'user.controller.e2e-spec.ts',
+      'user.create.e2e-spec.ts',
       'app.e2e-spec.ts',
       'auth.controller.e2e-spec.ts',
       'parking.controller.e2e-spec.ts',
       'user.update.e2e-spec.ts',
+      'get.parking.controller.e2e-spec.ts',
       'parkingSpot.controller.e2e-spec.ts',
+      'delete.parking.controller.e2e-spec.ts',
       'user.delete.e2e-spec.ts',
     ];
 
-    const sortedTests = tests.sort((a, b) => {
-      const aName = a.path.split('/').pop();
-      const bName = b.path.split('/').pop();
-      return order.indexOf(aName) - order.indexOf(bName);
+    const testMap = new Map();
+    tests.forEach((test) => {
+      const name = test.path.split('/').pop();
+      testMap.set(name, test);
     });
+
+    const sortedTests = order
+      .filter((name) => testMap.has(name))
+      .map((name) => testMap.get(name));
 
     return sortedTests;
   }
