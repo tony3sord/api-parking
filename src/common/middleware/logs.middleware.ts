d31ccece2819af7d62a2143930681bc033e6bd61
tony3sord <tressordtony@gmail.com>
2025-05-05 03:18:56 -0400

    feat: Enhance LoggerMiddleware to include user information in logs

--- src/common/middleware/logs.middleware.ts
+++ src/common/middleware/logs.middleware.ts
@@ -1,22 +1,33 @@
 import { Injectable, NestMiddleware, Logger } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 import { LogService } from '../logs/logs.service';
+import { AuthService } from 'src/modules/auth/service/auth.service';
+import { User } from 'src/modules/user/entities/user.entity';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
-  constructor(private readonly logService: LogService) {
+  constructor(
+    private readonly logService: LogService,
+    private readonly authService: AuthService,
+  ) {
     this.logService = logService;
   }
   private readonly logger = new Logger('HTTP');
 
-  use(req: Request, res: Response, next: NextFunction) {
+  async use(req: Request, res: Response, next: NextFunction) {
     const { method, originalUrl } = req;
+    let user: User | null = null;
+    const token: string | undefined =
+      req.headers['authorization']?.split(' ')[1];
+    if (token) {
+      user = await this.authService.verifyToken(token);
+    }
     const start = Date.now();
 
     res.on('finish', async () => {
       const { statusCode } = res;
       const duration = Date.now() - start;
-      const logMessage = `${method} ${originalUrl} ${statusCode} - ${duration}ms`;
+      const logMessage = `${method} ${originalUrl} ${statusCode} - ${duration}ms - ${user !== null ? user.username : null} - ${user !== null ? user.role : null}`;
       this.logger.log(logMessage);
       if (process.env.NODE_ENV !== 'test') {
         await this.logService.saveLog(
@@ -24,6 +35,7 @@ export class LoggerMiddleware implements NestMiddleware {
           originalUrl,
           statusCode,
           duration,
+          user !== null ? user.username : null,
         );
       }
     });
